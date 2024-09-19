0df3c788cef692f22b58f613c042cc4c9801eff9
Javier Preciado <jpreciado85@gmail.com>
2024-09-19 14:42:25 -0500

    sonidos interativos

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
         let currentRotation = puerta.getAttribute('rotation');
         let newYRotation = currentRotation.y + 90;
         puerta.setAttribute('rotation', {x: 0, y: newYRotation % 360, z: 0});
+        puerta.components.sound.playSound(); 
       });
     }
   
     if (caja) {
-      caja.addEventListener('click', () => {
-        let currentPosition = caja.getAttribute('position');
-        let newXPosition = currentPosition.x + 1;
-        caja.setAttribute('position', {x: newXPosition, y: currentPosition.y, z: currentPosition.z});
-        console.log('Caja movida a:', newXPosition);
-      });
-    }
+        caja.addEventListener('click', () => {
+          let currentPosition = caja.getAttribute('position');
+          let newXPosition = currentPosition.x + 1;
+          caja.setAttribute('position', {x: newXPosition, y: currentPosition.y, z: currentPosition.z});
+          console.log('Caja movida a:', newXPosition);
+          caja.components.sound.playSound();
+        });
+      }
   });
   
