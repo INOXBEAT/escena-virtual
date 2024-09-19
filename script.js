615332b9e03cb74c719d2395f0c1eb23eef3d3e2
Javier Preciado <jpreciado85@gmail.com>
2024-09-19 14:50:58 -0500

    cambio de color de los objetos

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const puerta = document.getElementById('puerta');
     const caja = document.getElementById('caja');
-  
+
     if (puerta) {
-      puerta.addEventListener('click', () => {
-        let currentRotation = puerta.getAttribute('rotation');
-        let newYRotation = currentRotation.y + 90;
-        puerta.setAttribute('rotation', {x: 0, y: newYRotation % 360, z: 0});
-        puerta.components.sound.playSound(); 
-      });
+        // Seleccionamos el primer a-box dentro de la puerta
+        const puertaBox = puerta.querySelector('a-box');
+
+        puerta.addEventListener('click', () => {
+            let currentRotation = puerta.getAttribute('rotation');
+            let newYRotation = currentRotation.y + 90;
+            puerta.setAttribute('rotation', {x: 0, y: newYRotation % 360, z: 0});
+
+            puertaBox.setAttribute('color', '#FF0000'); 
+            puerta.components.sound.playSound();
+        });
     }
-  
+
     if (caja) {
         caja.addEventListener('click', () => {
-          let currentPosition = caja.getAttribute('position');
-          let newXPosition = currentPosition.x + 1;
-          caja.setAttribute('position', {x: newXPosition, y: currentPosition.y, z: currentPosition.z});
-          console.log('Caja movida a:', newXPosition);
-          caja.components.sound.playSound();
+            let currentPosition = caja.getAttribute('position');
+            let newXPosition = currentPosition.x + 1;
+            caja.setAttribute('position', {x: newXPosition, y: currentPosition.y, z: currentPosition.z});
+            caja.setAttribute('material', 'color', '#00FF00'); // Cambiar el color a verde
+            caja.components.sound.playSound();
         });
-      }
-  });
-  
+    }
+});
